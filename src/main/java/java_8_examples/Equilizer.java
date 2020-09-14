f0e437ec8ff58d9cc0ad311440366dfcbf977d19
fedrfedra <vetervell@gmail.com>
2020-09-14 10:47:27 +0300

    count doubles Method takes lambda

---
+++ src/main/java/java_8_examples/Equilizer.java
@@ -0,0 +1,5 @@
+package java_8_examples;
+
+public interface Equilizer {
+    <T> Boolean areEqaual(T t0, T t1);
+}
--- src/main/java/java_8_examples/ListUtils.java
+++ src/main/java/java_8_examples/ListUtils.java
@@ -6,10 +6,10 @@ import java.util.List;
  * @author Evgeny Borisov
  */
 public class ListUtils {
-    public static <T> int countDuplicates(List<T> list, T t) {
+    public static <T> int countDuplicates(List<T> list, T t, Equilizer eq) {
         int counter=0;
         for (T t1 : list) {
-            if (t1.equals(t)) {
+            if (eq.areEqaual(t1, t)) {
                 counter++;
             }
         }
